bbfed105d618bdb7baae889d4f99b2e2bb3b36a6
vedavathi-b26 <vedavathi.b@westernacher.com>
2024-01-18 12:39:43 +0530

    ngResource error fix for button,header,page - added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-//import { TaskModule } from './components/task.module';
 import { NgxsModule } from '@ngxs/store';
 import { NgxsReduxDevtoolsPluginModule } from '@ngxs/devtools-plugin';
 import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
 import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
-//import InboxScreenComponent from './components/inbox-screen.component';
-//import PureInboxScreenComponent from './components/pure-inbox-screen.component';
-//import { AvatarComponent } from './avatar/avatar.component';
 import { FormsModule } from '@angular/forms';
 import { SelectComponent } from './select/select.component';
 import { IconButtonComponent } from './icon-button/icon-button.component';
 import { LinkComponent } from './link/link.component';
 import { RadioComponent } from './radio/radio.component';
 import { TextareaComponent } from './textarea/textarea.component';
-
+import Button from '../stories/button.component';
+import Header from '../stories/header.component';
+import Page from '../stories/page.component';
 
 @NgModule({
-  declarations: [AppComponent, LinkComponent, TextareaComponent, RadioComponent /*InboxScreenComponent, PureInboxScreenComponent,*/ /*AvatarComponent*/, SelectComponent, IconButtonComponent],
+  declarations: [
+    AppComponent,
+    SelectComponent, 
+    IconButtonComponent,
+    LinkComponent,
+    RadioComponent, 
+    TextareaComponent, 
+    Button,
+    Header,
+    Page
+  ],
   imports: [
     BrowserModule,
     FormsModule,
-    //TaskModule,
     NgxsModule.forRoot([], { developmentMode: !environment.production, }),
     NgxsReduxDevtoolsPluginModule.forRoot(),
     NgxsLoggerPluginModule.forRoot({ disabled: environment.production, }),
@@ -30,4 +37,4 @@ import { TextareaComponent } from './textarea/textarea.component';
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
