9be4d0889536c783b30cd5ddcdac6f52bf70e6b2
vedavathi-b26 <vedavathi.b@westernacher.com>
2024-01-29 19:50:41 +0530

    Unit test cases changes - Added

--- src/app/button/button.stories.ts
+++ src/app/button/button.stories.ts
@@ -1,6 +1,6 @@
 
 //import { moduleMetadata } from '@storybook/angular';
-import {Meta, StoryObj, moduleMetadata } from '@storybook/angular';
+/*import {Meta, StoryObj, moduleMetadata } from '@storybook/angular';
 import { ButtonComponent } from './button.component';
 import ButtonVariantType from '../types/ButtonVariantType';
 import  ThemeType  from "../types/ThemeType";
@@ -26,6 +26,29 @@ export const Playground:  Story = {
       variant: ButtonVariantType.default,
     },
   }),
+};*/
+
+
+import {Meta, Story } from '@storybook/angular';
+import { ButtonComponent } from './button.component';
+import ButtonVariantType from '../types/ButtonVariantType';
+import  ThemeType  from "../types/ThemeType";
+//import {ButtonComponentSpec} from './button.component.spec';
+
+
+export default{
+  title: 'Form Elements/Button',
+  component: ButtonComponent,
+} as Meta;
+
+const Template: Story<ButtonComponent> = (args: ButtonComponent) => ({
+  props:args,
+});
+
+export const Playground = Template.bind ({});
+Playground.args = {
+  label:'Lorem ipsum',
+  theme: ThemeType.primary,
+  variant: ButtonVariantType.default,
 };
-
-
+  
