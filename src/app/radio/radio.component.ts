043824fd8241ffffc8115060efa05566f430c950
Akanksha Raj <akanksha.raj@westernacher.com>
2024-09-19 14:46:13 +0530

    features-added-to-radio-component

--- src/app/radio/radio.component.ts
+++ src/app/radio/radio.component.ts
@@ -1,14 +1,24 @@
-import { Component, EventEmitter, Input, Output} from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import ThemeType from '../types/ThemeType';
 
 @Component({
-    selector:'app-radio',
-    templateUrl:'./radio.component.html',
-    styleUrls:['./radio.component.scss'],
+    selector: 'app-radio',
+    templateUrl: './radio.component.html',
+    styleUrls: ['./radio.component.scss'],
 })
 
 export class RadioComponent {
+    @Input() id: string = '';
     @Input() label: string = '';
     @Input() type!: string;
-    @Input() theme: ThemeType = ThemeType.default;  
+    @Input() theme: 'default' | 'primary' | 'secondary' | 'success' | 'warning' | 'danger' = 'default';
+    @Input() checked !: boolean;
+
+    @Output() onChange = new EventEmitter<boolean>();
+
+    onRadioChange(event: Event) {
+        const target = event.target as HTMLInputElement;
+        this.checked = target.checked;
+        this.onChange.emit(this.checked);
+    }
 }
--- src/app/radio/radio.stories.ts
+++ src/app/radio/radio.stories.ts
@@ -1,28 +1,38 @@
 import {Meta, StoryObj, moduleMetadata } from '@storybook/angular';
 import { RadioComponent } from './radio.component';
 import { action } from '@storybook/addon-actions';
-import  ThemeType  from "../types/ThemeType";
+// import  ThemeType  from "../types/ThemeType";
 
-const meta: Meta<typeof RadioComponent> = {
+const meta: Meta<RadioComponent> = {
   
     title: 'Form Elements/Radio',
     component: RadioComponent,
     tags:["autodocs"],
     argTypes: {
+      theme: {
+        control:{
+          type:'select'
+        },
+        options: ['default', 'primary', 'secondary', 'success', 'warning', 'danger']
+      }
     },
-  } as Meta;
+  } as Meta<RadioComponent>;
   
   export default meta;
-  type Story = StoryObj<typeof RadioComponent>;
+  type Story = StoryObj<RadioComponent>;
 
   export const Playground:  Story = {
-    render:() => ({
+    render:(args) => ({
       props:{
-        theme: ThemeType.primary,
+        ...args,
         label:'Lorem ipsum',
-        onChange: action('Radio changed'),
+        onChange: action('onChange'),
       },
     }),
+    args:{
+      theme: 'primary',
+      checked: true
+    }
   };
 
 
