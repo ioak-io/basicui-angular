8e5850f62143baad3bf17e3d0c82b89bf3cbd56b
Akanksha Raj <akanksha.raj@westernacher.com>
2024-10-01 13:23:31 +0530

    select-modalFooter-changes

--- src/app/select/select.component.ts
+++ src/app/select/select.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-select',
@@ -6,9 +6,62 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./select.component.scss']
 })
 export class SelectComponent {
-  [x: string]: any;
-  @Input() placeholder = '';
-  @Input() selectType: 'single' | 'autocomplete' = 'single';
-  @Input() options: { value: string, label: string }[] = [];
-  selectedValue: string = '';
+  // [x: string]: any;
+  @Input() placeholder = 'Select Dropdown';
+  @Input() selectType: 'single' | 'multiple' | 'autocomplete' = 'single';
+  @Input() options: { value: string | number, label: string }[] = [];
+  @Input() allowNewValues = false;
+  @Input() autocomplete = false;
+
+  value: (string | number)[] = [];
+
+  searchText: string = '';
+  isVisible: boolean = false;
+  filteredOptions: { value: string | number, label: string }[] = [];
+
+  ngOnInIt() {
+    this.filteredOptions = this.options;
+  }
+
+  ngOnChanges() {
+    this.filteredOptions = this.options;
+  }
+
+  toggleDropdown() {
+    this.isVisible = !this.isVisible;
+  }
+
+  handleOptionClick(optionValue: string | number) {
+    if (this.selectType === 'single') {
+      this.value = [optionValue];
+      this.isVisible = false;
+    } else {
+      if (this.value.includes(optionValue)) {
+        this.value = this.value.filter(item => item != optionValue);
+      }
+      else {
+        this.value = [...this.value, optionValue];
+      }
+    }
+
+  }
+
+  onSearchTextChange(searchText: string) {
+    const _searchText = searchText.toLowerCase();
+    if (!_searchText.trim()) {
+      this.filteredOptions = this.options;
+    } else {
+      this.filteredOptions = this.options.filter(item => (item.label + '').toLowerCase().includes(_searchText));
+
+      if (this.allowNewValues && !this.filteredOptions.find(item => item.label === searchText)) {
+        this.filteredOptions.unshift({ label: searchText, value: searchText });
+      }
+    }
+
+  }
+
+  getSelectedtext(): string{
+    return this.value.map(val => this.options.find(option => option.value === val)?.label || val).join(', ');
+  }
 }
+
